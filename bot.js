257ea91e2b12be82f59f92ddb15f2467106e730b
myAmericanShoes <44378580+myAmericanShoes@users.noreply.github.com>
2019-08-11 20:09:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@
 
 var regID = "Bot regID is: 10x24x19xAA";
 var statusFlag = 0;
-const captureCompare = [/^\eric$/, /^\Eric$/, /^\overlord$/, /^\Overlord$/, /^\\reg$/, /^\\status$/, /^\@Eric$/, /^\Prepare for trouble$/, /^\Josh$/, /^\Coolest$/, /^\coolest$/, /^\Happy New Year!$/, /^\Raphtalia$/];
-const responseGrid = ["God damn!", "God damn", "Ainz-Sama!", "Ainz-Sama!", regID, "status", "@God_damn", "And make it double", "Josh-Sama!", "God-Damn-Coolest-O", "God-Damn-Coolest-O", "Reminder: Happy God Damn New Year!", "NAOFUMI-SAMA!"];
+const captureCompare = [/^\Nico$/, /^\nico$/, /^\overlord$/, /^\Overlord$/, /^\\reg$/, /^\\status$/, /^\@Eric$/, /^\Prepare for trouble$/, /^\Josh$/, /^\Coolest$/, /^\coolest$/, /^\Happy New Year!$/, /^\Raphtalia$/];
+const responseGrid = ["I like rocks XD", "I like rocks XD", "Ainz-Sama!", "Ainz-Sama!", regID, "status", "@God_damn", "And make it double", "Josh-Sama!", "God-Damn-Coolest-O", "God-Damn-Coolest-O", "Reminder: Happy God Damn New Year!", "NAOFUMI-SAMA!"];
 
 
 /***************************/
